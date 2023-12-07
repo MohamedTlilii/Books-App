fa3752881593eb8bc272bb60c7f9d611dbe0bbab
NAUT5 <Mohamedtlili1995@hotmail.com>
2023-12-07 16:07:55 +0100

    almost-done

--- src/Pages/Book.jsx
+++ src/Pages/Book.jsx
@@ -4,21 +4,21 @@ import Card from "react-bootstrap/Card";
 function Book() {
   return (
     <div className="page-book-page">
-      <Card className="Card" style={{ width: "18rem" }}>
-        <Card.Img width="500" variant="top" src="" />
-        <Card.Body>
-          <Card.Text></Card.Text>
-        </Card.Body>
+      <Card className="Card">
+        <Card.Img width="500" vraiant="top" src={book?.img} />
+        <CardBody>
+          <Card.Text> {book?.desc}</Card.Text>
+        </CardBody>
       </Card>
       <div>
-        <h1>{}</h1>
-        <h4>{}</h4>
-        <h6> Price : {} $</h6>
+        <h1>{book.title}</h1>
+        <h4>{book.author}</h4>
+        <h6> Price : {book.price} $</h6>
       </div>
+
       <h1>Single book</h1>
     </div>
   );
 }
 
 export default Book;
-
--- src/Pages/Books.jsx
+++ src/Pages/Books.jsx
@@ -1,9 +1,23 @@
 import React from "react";
+import {books} from "../Data";
+import Card from "react-bootstrap/Card"
+import { Link } from "react-router-dom";
 
 function Books() {
   return (
-    <div >
-      <h1>Books</h1>
+    <div className="page" >
+{books.map((book)=>(
+  <Card  style={{ width: "18rem", height:"500px" }}>
+  <Card.Img width="500" variant="top" src={books.img} />
+  <Card.Body>
+    <Card.Title>{books.title}</Card.Title>
+    <Card.Link>
+      <Link to={`/book/${book.id}`}>See more</Link>
+    </Card.Link>
+  </Card.Body>
+</Card>
+))}
+      
     </div>
   );
 }
